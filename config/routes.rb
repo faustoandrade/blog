030de26a24b7219d2197708db34c024d66325e2b
faustoandrade <ayf1207@gmail.com>
2019-07-31 01:15:59 -0500

    FA- se genero un controlador articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # direccion de la pagina
-  get 'welcome/index' # debemos poner en la url welcome/index para que nos direccione a ella.
+  # get 'welcome/index' # debemos poner en la url welcome/index para que nos direccione a ella.
   root 'welcome#index' # nos direcciona directamente a la pagina.
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles # globaliza todas las rutas: get, post, delete, patch, put
 end
