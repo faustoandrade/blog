fe574abb3dda484b6913011f0dc99b2cb9672de3
faustoandrade <ayf1207@gmail.com>
2019-07-31 00:14:40 -0500

    FA- definiendo la pagina en todas las vistas por medio de layouts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  # direccion de la pagina
   get 'welcome/index' # debemos poner en la url welcome/index para que nos direccione a ella.
-  root 'welcome#index' # nos direcciona directamente a la pagina. 
+  root 'welcome#index' # nos direcciona directamente a la pagina.
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
