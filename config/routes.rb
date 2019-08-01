05f89794d07ba8428c1e6b19d2adef6945a968d1
faustoandrade <ayf1207@gmail.com>
2019-08-01 14:03:56 -0500

    FA- organized routes and authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  # root 'welcome#index' # nos direcciona directamente a la pagina.
 
+  # get 'sessions/new'
   resources :articles # globaliza todas las rutas: get, post, delete, patch, put
   resources :comments
   devise_for :users
-  devise_for :models
-  devise_for :installs
-
   # direccion de la pagina
   # get 'welcome/index' # debemos poner en la url welcome/index para que nos direccione a ella.
-  root 'welcome#index' # nos direcciona directamente a la pagina.
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index' # nos direcciona directamente a la pagina.
+
 end
